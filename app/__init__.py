1b0f032f6ceff1f67c75e601ba8981667ac52ba6
Kan Tippayamontri <kantippayamontri@gmail.com>
2024-04-23 14:58:13 +0700

    make ui beautiful

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
-import networkx as nx
 import matplotlib.pyplot as plt
 from icecream import ic
 import graphviz
 import random
 import string
+
 
 def create_app():
     app = Flask(
@@ -12,13 +12,19 @@ def create_app():
     )
     type_device = ["Security Camera", "Smartmetre", "Light"]
 
+    type_device_details = {
+        "Security Camera": ["Footage"],
+        "Smartmetre": ["Energy Usage"],
+        "Light": ["Light Status", "Color"],
+    }
+
     cate_raw_data = ["low", "medium", "high"]
 
     cate_service = ["low", "medium", "high"]
 
     action = ["average", "anonymise", "transfer"]
 
-    service_type= [
+    service_type = [
         "Advertising Company",
         "Health Tracking Service",
         "Medical Service",
@@ -46,38 +52,48 @@ def create_app():
             _service_type = request.form["service_type"]
 
             tree_data = {
-                "type_device":{"data":type_device, "choose":_type_device},
-                "cate_raw_data":{"data":cate_raw_data, "choose": _cate_raw_data},
-                "cate_service": {"data":cate_service, "choose":_cate_service},
-                "action": {"data":action, "choose":_action},
-                "service_type": {"data":service_type , "choose":_service_type},
+                "type_device": {"data": type_device, "choose": _type_device},
+                "cate_raw_data": {"data": cate_raw_data, "choose": _cate_raw_data},
+                "cate_service": {"data": cate_service, "choose": _cate_service},
+                "action": {"data": action, "choose": _action},
+                "service_type": {"data": service_type, "choose": _service_type},
             }
 
-            ic(tree_data)
-            
+            # ic(tree_data)
+
             graph_img_filename = create_graph(data=tree_data)
-            print(f"graph_img_filename: {graph_img_filename}")
+            # print(f"graph_img_filename: {graph_img_filename}")
 
             # print(f"tree_data: ", tree_data)
 
-            return "".join(
-                [
-                    f"type of device : {_type_device}<br>",
-                    f"cate of raw data : {_cate_raw_data}<br>",
-                    f"cate of cate service: {_cate_service}<br>",
-                    f"cate of action: {_action}<br>",
-                    f"cate of service type: {_service_type}<br>",
-                ]
-            )
-    
-    def create_graph(data:dict):
-        
+            # return "".join(
+            #     [
+            #         f"type of device : {_type_device}<br>",
+            #         f"cate of raw data : {_cate_raw_data}<br>",
+            #         f"cate of cate service: {_cate_service}<br>",
+            #         f"cate of action: {_action}<br>",
+            #         f"cate of service type: {_service_type}<br>",
+            #     ]
+            # )
+            return render_template("show_graph.html", graph_filename=graph_img_filename)
+
+    def create_graph(data: dict):
+
         graph = graphviz.Digraph(comment="databank", format="png")
 
         for category, category_data in data.items():
-            chosen_value = next((item for item in category_data["data"] if item == category_data["choose"]), None)
-            category_name = category.replace("_", " ").capitalize()  
-            label = f"{category_name} : {chosen_value}" if chosen_value else category_name  
+            chosen_value = next(
+                (
+                    item
+                    for item in category_data["data"]
+                    if item == category_data["choose"]
+                ),
+                None,
+            )
+            category_name = category.replace("_", " ").capitalize()
+            label = (
+                f"{category_name} : {chosen_value}" if chosen_value else category_name
+            )
             graph.node(category, label=label, shape="box")
 
         keys = list(data.keys())
@@ -85,7 +101,8 @@ def create_app():
             graph.edge(keys[i], keys[i + 1])
 
         filename = generate_random_filename()
-        graph.render(filename=filename, format="png", view=True)
+        filename_path = f"./app/static/{filename}"
+        graph.render(filename=filename_path, format="png", view=False)
         return f"{filename}.png"
 
     def generate_random_filename(length=15):
@@ -103,20 +120,27 @@ def create_app():
         allowed_chars = string.ascii_lowercase + string.digits + string.ascii_uppercase
 
         # Generate a random string of the specified length
-        random_string = ''.join(random.choice(allowed_chars) for i in range(length))
+        random_string = "".join(random.choice(allowed_chars) for i in range(length))
 
         # Ensure the filename starts with a letter (avoid issues with some OS file systems)
         if not random_string[0].isalpha():
-            random_string = random.choice(string.ascii_lowercase + string.ascii_uppercase) + random_string[1:]
+            random_string = (
+                random.choice(string.ascii_lowercase + string.ascii_uppercase)
+                + random_string[1:]
+            )
 
         return f"graph_{random_string}"
 
+    @app.route("/clear")
+    def clear_img():
+        return "clear image success."
+
     @app.route("/hello")
     def hello():
-
         return "Hello, world"
 
     return app
+
 
 if __name__ == "__main__":
     app = create_app()
