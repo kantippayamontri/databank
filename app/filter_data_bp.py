e91cdae52ff533ecb4f1e2d59af8bb081f83aff7
jame25193 <weerasak.chiang@gmail.com>
2024-07-23 22:48:56 +0700

    fix new data

--- app/filter_data_bp.py
+++ app/filter_data_bp.py
@@ -14,25 +14,25 @@ bp = Blueprint("filter", __name__, url_prefix="/filter")
 data = {
     "devices": {
         "0": {
-            "device_name": "Smart TV",
-            "device_type": "Digital TV",
+            "device_name": "Gauge",
+            "device_type": "Smartmetre",
             "device_unprocessed": [
-                "Light status",
-                "Energy usage",
-                "Colour"
+                "Footage",
+                "gauge value",
+                "Light status"
             ],
             "raw_data": {
-                "Colour": {
-                    "action": "Anonymise",
-                    "frequency": "Yearly",
-                    "sensitivity": "Medium"
-                },
-                "Energy usage": {
-                    "action": "Transfer",
-                    "frequency": "Yearly",
-                    "sensitivity": "Medium"
+                "Footage": {
+                    "action": "Average",
+                    "frequency": "Daily",
+                    "sensitivity": "Low"
                 },
                 "Light status": {
+                    "action": "Average",
+                    "frequency": "Daily",
+                    "sensitivity": "Low"
+                },
+                "gauge value": {
                     "action": "Average",
                     "frequency": "Daily",
                     "sensitivity": "Low"
@@ -40,25 +40,13 @@ data = {
             }
         },
         "1": {
-            "device_name": "Gauge",
-            "device_type": "Light",
+            "device_name": "smart meter",
+            "device_type": "Smartmetre",
             "device_unprocessed": [
-                "Light Status",
-                "Color",
-                "Activity period"
+                "Energy usage"
             ],
             "raw_data": {
-                "Activity period": {
-                    "action": "Transfer",
-                    "frequency": "No fix time",
-                    "sensitivity": "Medium"
-                },
-                "Color": {
-                    "action": "Upload",
-                    "frequency": "Yearly",
-                    "sensitivity": "High"
-                },
-                "Light Status": {
+                "Energy usage": {
                     "action": "Average",
                     "frequency": "Daily",
                     "sensitivity": "Low"
@@ -68,92 +56,62 @@ data = {
     },
     "services": {
         "0": {
-            "service_name": "The8thFloor",
-            "service_type": "Tech Company",
             "cate_service": {
-                "0": { 
-                    "device_id": 0,
-                    "device_unprocessed": "Colour",
-                    "action": "Report Data",
-                    "category": "Low",
-                    "frequency": "Daily"
+                "0": {
+                    "Footage": {
+                        "action": "View Data",
+                        "category": "Low",
+                        "frequency": "Daily"
+                    },
+                    "Light status": {
+                        "action": "View Data",
+                        "category": "Low",
+                        "frequency": "Daily"
+                    }
                 },
-                "1":{
-                    "device_id": 0,
-                    "device_unprocessed": "Type Status",
-                    "action": "View Data",
-                    "category": "Low",
-                    "frequency": "Daily"
+                "1": {
+                    "Energy usage": {
+                        "action": "View Data",
+                        "category": "Low",
+                        "frequency": "Daily"
+                    }
                 }
-            }
+            },
+            "service_name": "The8thFloor",
+            "service_type": "Tech Company"
         },
         "1": {
-            "service_name": "JameCompany",
-            "service_type": "Security Company",
             "cate_service": {
-                "0": { 
-                    "device_id": 1,
-                    "device_unprocessed": "Colour",
-                    "action": "Read Data",
-                    "category": "Low",
-                    "frequency": "Daily"
-                },
-                "1": { 
-                    "device_id": 0,
-                    "device_unprocessed": "Light Status",
-                    "action": "View Data",
-                    "category": "Low",
-                    "frequency": "Daily"
-                },
-                "2": { 
-                    "device_id": 0,
-                    "device_unprocessed": "New Status",
-                    "action": "View Data",
-                    "category": "Low",
-                    "frequency": "Daily"
+                "0": {
+                    "Footage": {
+                        "action": "View Data",
+                        "category": "Low",
+                        "frequency": "Daily"
+                    }
                 }
-            }
-        },
-        "2": {
-            "service_name": "Techneena",
-            "service_type": "Tech Company",
-            "cate_service": {
-                "0": { 
-                    "device_id": 0,
-                    "device_unprocessed": "Colour",
-                    "action": "Report Data",
-                    "category": "Low",
-                    "frequency": "Daily"
-                },
-                "1":{
-                    "device_id": 0,
-                    "device_unprocessed": "Show me Status",
-                    "action": "View Data",
-                    "category": "Low",
-                    "frequency": "Daily"
-                }
-            }
+            },
+            "service_name": "Meta",
+            "service_type": "Insurance"
         }
     }
 }
 
 @bp.route("/show_data", methods=["GET"])
 def form():
-    print('hello')
-    data_in_dropdown = []
-    if session["device"]["device_type"] in type_device_details.keys():
-        data_in_dropdown = type_device_details[session["device"]["device_type"]]
-    else:
-        data_in_dropdown = unprocessed_data
+    # data_in_dropdown = []
+    # if session["device"]["device_type"] in type_device_details.keys():
+    #     data_in_dropdown = type_device_details[session["device"]["device_type"]]
+    # else:
+    #     data_in_dropdown = unprocessed_data
     
     return render_template(
         "forms/filter_data_form.html",
-        form_utils={
-            "device": {
-                "type_device": enumerate(type_device),
-                "unprocessed_data": enumerate(data_in_dropdown),
-            }
-        },
+        # form_utils={
+        #     "device": {
+        #         "type_device": enumerate(type_device),
+        #         "unprocessed_data": enumerate(data_in_dropdown),
+        #     }
+        # },
     )
 
 @bp.route("/filter", methods=["GET"])
