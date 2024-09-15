0034b73252de94c18b55a113603d1b02e862699f
jame25193 <weerasak.chiang@gmail.com>
2024-09-15 20:36:56 +0700

    device show

--- app/cate_raw_data_bp.py
+++ app/cate_raw_data_bp.py
@@ -21,7 +21,6 @@ def get():
 @bp.route("/form/<int:device_id>", methods=["GET", "POST"])
 def form(device_id):
     if request.method == "POST":
-
         # print(f"data from form : {request.form}")
         # yes = "yes"
         # no = "no"
@@ -67,6 +66,41 @@ def form(device_id):
         )
 
     return "Cate Raw Data Form"
+
+@bp.route("/ajax/<int:device_id>", methods=["POST"])
+def ajax(device_id):
+    print(f"data from form : {request.form}")
+    # print(f"data from form : {request.form}")
+    # yes = "yes"
+    # no = "no"
+    # for d in request.form:
+    #     print(
+    #         f"{d} : {request.form[d]} , action: {yes if 'action_' in d else no }, frequency: {yes if 'frequency_' in d else no}"
+    #     )
+    data = session["devices"][str(device_id)]["raw_data"]
+    raw_data = request.form['raw_data']
+    data[raw_data] = {"action": "" , "frequency" : "", "sensitivity" : ""}
+    data[raw_data]["action"] = request.form["action"]
+    data[raw_data]["frequency"] = request.form["frequency"]
+    data[raw_data]["sensitivity"] = request.form["sensitivity"]
+
+    # print(f"data: {data}")
+    
+    session["devices"][str(device_id)]["raw_data"] = data
+
+    return session["devices"][str(device_id)]["raw_data"]
+
+@bp.route("/getData/<int:device_id>", methods=["GET"])
+def getData(device_id):
+    data = {
+        "device_id":str(device_id),   
+        "frequency":frequency,
+        "action":raw_data_action,
+        "sensitivity":cate_raw_data,
+        "raw_data":session["devices"][str(device_id)]["raw_data"]
+    }
+    return data
+
 
 
 @bp.route("/delete", methods=["GET"])
